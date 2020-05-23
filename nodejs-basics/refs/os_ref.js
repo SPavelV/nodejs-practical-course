4bd5b91cd016ada54e534a769c63023bd57bf593
Solovev Pavel <p.solovev@elewise.com>
2020-05-23 11:21:13 +0400

    module OS

---
+++ nodejs-basics/refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+// Platform
+console.log(os.platform());
+
+// Architecture
+console.log(os.arch());
+
+// Information
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// All memory
+console.log(os.totalmem());
+
+// Root dir
+console.log(os.homedir());
+
+// Work time
+console.log(os.uptime());
