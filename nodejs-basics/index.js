19a35cdb2daeb517ffe1e0b6109221f788cd402e
Solovev Pavel <p.solovev@elewise.com>
2020-05-23 15:44:06 +0400

    web server with html pages

--- nodejs-basics/index.js
+++ nodejs-basics/index.js
@@ -1,24 +1,39 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  
   if (req.method === "GET") {
     res.writeHead(200, {
-      "Content-type": "text/html",
+      "Content-type": "text/html; charset=utf-8",
     });
 
-    res.end(`
-      <h1>Form</h1>
-      <form method="post" action="/">
-        <input type="text" name="title" />
-        <button type="submit">Send</button>
-      </form>
-    `);
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "views", "index.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+
+          res.end(content);
+        }
+      );
+    } else if (req.url === "/about") {
+      fs.readFile(
+        path.join(__dirname, "views", "about.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+
+          res.end(content);
+        }
+      );
+    }
   } else if (req.method === "POST") {
     const body = [];
     res.writeHead(200, {
-      "Content-type": "text/html; charset=utf-8"
-    })
+      "Content-type": "text/html; charset=utf-8",
+    });
 
     req.on("data", (data) => {
       body.push(Buffer.from(data));
