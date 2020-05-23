db777c6b078b0fc06b616433081e0da0e731fda1
Solovev Pavel <p.solovev@elewise.com>
2020-05-23 11:05:33 +0400

    module - fs

--- nodejs-basics/refs/fs_ref.js
+++ nodejs-basics/refs/fs_ref.js
@@ -9,31 +9,41 @@ const path = require('path')
   
 // })
 
-fs.writeFile(
-  path.join(__dirname, 'notes', 'mynotes.txt'), 
-  'Hello world',
-  (err) => {
-    if(err) throw err
-    console.log('Файл был создан')
+// fs.writeFile(
+//   path.join(__dirname, 'notes', 'mynotes.txt'), 
+//   'Hello world',
+//   (err) => {
+//     if(err) throw err
+//     console.log('Файл был создан')
 
-    fs.appendFile(
-      path.join(__dirname, 'notes', 'mynotes.txt'),
-      ' From append file',
-      err => {
-        if(err) throw err
-        console.log('Файл был изменен')
-      
-      }
-    )
-  }
-)
+//     fs.appendFile(
+//       path.join(__dirname, 'notes', 'mynotes.txt'),
+//       ' From append file',
+//       err => {
+//         if(err) throw err
+//         console.log('Файл был изменен')
+        
+//         fs.readFile(
+//           path.join(__dirname, 'notes', 'mynotes.txt'),
+//           'utf-8',
+//           (err, data) => {
+//             if (err) throw err
+//             console.log(data)
+            
+//           }
+//         )
+        
+//       }
+//     )
+//   }
+// )
 
-fs.readFile(
+fs.rename(
   path.join(__dirname, 'notes', 'mynotes.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err
-    console.log(data)
-    
+  path.join(__dirname, 'notes', 'notes.txt'),
+
+  err => {
+    if(err) throw err
+    console.log('Файл переименован')
   }
 )
