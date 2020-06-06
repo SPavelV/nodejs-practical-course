38f951e020b6d500f0f0ff8645d706016aad3cfd
Solovev Pavel <p.solovev@elewise.com>
2020-06-06 17:30:05 +0400

    transform data in client

--- node-express/models/course.js
+++ node-express/models/course.js
@@ -1,19 +1,28 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const course = new Schema({
+const courseSchema = new Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   price: {
     type: Number,
-    required: true
+    required: true,
   },
   img: String,
   userId: {
     type: Schema.Types.ObjectId,
-    ref: "User"
-  }
+    ref: "User",
+  },
 });
 
-module.exports = model("Course", course);
+courseSchema.method("toClient", function () {
+  const course = this.toObject();
+
+  course.id = cours._id;
+  delete course._id;
+
+  return course;
+});
+
+module.exports = model("Course", courseSchema);
