105b756628f08f48eaf2db3b6b0b389a7dac2ff2
Solovev Pavel <p.solovev@elewise.com>
2020-06-06 17:22:23 +0400

    remove cart

--- node-express/routes/card.js
+++ node-express/routes/card.js
@@ -22,9 +22,14 @@ router.post("/add", async (req, res) => {
 });
 
 router.delete("/remove/:id", async (req, res) => {
-  const card = await Card.remove(req.params.id);
+  await req.user.removeFromCart(req.params.id);
+  const user = await req.user.populate("cart.items.courseId").execPopulate();
 
-  res.status(200).json(card);
+  const courses = mapCartItems(user.cart);
+  const cart = {
+    courses, price: computePrice(courses)
+  }
+  res.status(200).json(cart);
 });
 
 router.get("/", async (req, res) => {
