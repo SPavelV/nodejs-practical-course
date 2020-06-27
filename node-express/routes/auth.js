082611bf119e50c4ea617309155dd2f636bd8683
Solovev Pavel <p.solovev@elewise.com>
2020-06-27 16:53:17 +0400

    change password

--- node-express/routes/auth.js
+++ node-express/routes/auth.js
@@ -145,4 +145,29 @@ router.post("/reset", (req, res) => {
   }
 });
 
+router.post("/password", async(req, res) => {
+  try {
+    const user = await User.findOne({
+      _id: req.body.userId,
+      resetToken: req.body.token,
+      resetTokenExp: req.body.token,
+      resetTokenExp: {$gt: Date.now()}
+    });
+
+    if (user) {
+      user.password = await bcrypt.hash(req.body.password, 10);
+      user.resetToken = undefined;
+      user.resetTokenExp = undefined;
+      await user.save();
+      res.redirect("auth/login");
+    } else {
+      req.flash("loginError", "Время жизни токена истекло");
+      res.redirect("auth/login");
+    }
+  } catch (error) {
+    console.log(error);
+    
+  }
+})
+
 module.exports = router;
