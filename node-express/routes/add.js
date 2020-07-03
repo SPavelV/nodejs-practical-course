a965d92333eca8dafa6bd1ab03384b17556b83ff
Solovev Pavel <p.solovev@elewise.com>
2020-07-03 20:29:30 +0400

    courses validation

--- node-express/routes/add.js
+++ node-express/routes/add.js
@@ -2,14 +2,13 @@ const { Router } = require("express");
 const { validationResult } = require("express-validator/check");
 const Course = require("../models/course");
 const auth = require("../middleware/auth");
-const { courseValidators } = require("../utils/validators");
+const { courseValidators } = require("../utils-helpers/validators");
 const router = Router();
 
 router.get("/", auth, (req, res) => {
   res.render("add", {
     title: "Добавить курс",
-    isAdd: true,
-    error: errors()[0].msg,
+    isAdd: true
   });
 });
 
@@ -20,6 +19,12 @@ router.post("/", auth, courseValidators, async (req, res) => {
     return res.status(422).render("add", {
       title: "Добавить курс",
       isAdd: true,
+      error: errors.array()[0].msg,
+      data: {
+        title: req.body.title,
+        price: req.body.price,
+        img: req.body.img
+      }
     });
   }
 
--- node-express/routes/courses.js
+++ node-express/routes/courses.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
 const Course = require("../models/course");
-const router = Router();
 const auth = require("../middleware/auth");
+const { courseValidators } = require("../utils-helpers/validators");
+const { validationResult } = require("express-validator/check");
+
+const router = Router();
 
 function isOwner(course, req) {
   return course.userId.toString() === req.user._id.toString();
@@ -43,9 +46,16 @@ router.get("/:id/edit/", auth, async (req, res) => {
   }
 });
 
-router.post("/edit", auth, async (req, res) => {
+router.post("/edit", auth, courseValidators, async (req, res) => {
+  const errors = validationResult(req);
+  const { id } = req.body;
+  
+  if (!errors.isEmpty()) {
+    return res.status(422).redirect(`/courses/${id}/edit?allow=true`);
+  }
+
   try {
-    const { id } = req.body;
+   
     delete req.body.id;
     const course = await Course.findById(id);
     if (!isOwner(course, req)) {
