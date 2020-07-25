1b716b871339d0b7cb8bcd4b15c999072fe1de9a
Solovev Pavel <p.solovev@elewise.com>
2020-07-25 12:17:02 +0400

    protected keys

--- node-express/keys/index.js
+++ node-express/keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-  MONGODB_URI: "mongodb+srv://pavel:qbvQUmUy0744LTaV@cluster0-ximsm.mongodb.net/shop",
-  SESSION_SECRET: "some secret value",
-  SENDGRID_API_KEY: "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",
-  EMAIL_FROM: "p.solovev@elewise.com",
-  BASE_URL: "http://localhost:3000/"
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys.prod");
+} else {
+  module.exports = require("./keys.dev")
 }
