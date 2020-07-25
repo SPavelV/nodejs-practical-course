0bd1fcb1f98694efc3f9e15d82a77c6b3a4db2dc
Solovev Pavel <p.solovev@elewise.com>
2020-07-25 13:52:43 +0400

    update deploy config

--- node-express/keys/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URI: "mongodb+srv://pavel:qbvQUmUy0744LTaV@cluster0-ximsm.mongodb.net/shop",
-    SESSION_SECRET: "some secret value",
-    SENDGRID_API_KEY: "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",
-    EMAIL_FROM: "p.solovev@elewise.com",
-    BASE_URL: "http://localhost:3000/"
-  }
