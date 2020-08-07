f77f09b4ddb803930e129153f511208937bc42d2
Solovev Pavel <p.solovev@elewise.com>
2020-08-07 18:24:51 +0400

    add model

---
+++ node-mysql/models/todo.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../utils/database");
+const todo = sequelize.define("Todo", {
+  id: {
+    primaryKey: true,
+    autoIncremnt: true,
+    allowNull: false,
+    type: Sequelize.INTEGER,
+  },
+  done: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  date: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+});
+
+module.exports = todo;
