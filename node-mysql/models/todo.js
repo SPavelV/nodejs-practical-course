b9d96d13e23a26262d144e691fb48e86661bb365
Solovev Pavel <p.solovev@elewise.com>
2020-08-07 18:54:08 +0400

    create task

--- node-mysql/models/todo.js
+++ node-mysql/models/todo.js
@@ -14,11 +14,7 @@ const todo = sequelize.define("Todo", {
   title: {
     type: Sequelize.STRING,
     allowNull: false,
-  },
-  date: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
+  }
 });
 
 module.exports = todo;
